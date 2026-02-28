08dc2ce389c00d07aa5d7adac89dde956e438235
Matheus <Hazelramahz@icloud.com>
2026-02-28 15:20:39 -0300

    refactor(api): centralize dependency injection and swagger configuration

---
+++ FinancialGoalManager.API/Extensions/ServiceConfiguration.cs
@@ -0,0 +1,56 @@
+using FinancialGoalManager.Application.Commands.FinancialGoals.RegisterGoal;
+using FinancialGoalManager.Core.Repositories;
+using FinancialGoalManager.Infrastructure.Persistence.Repositories;
+using Microsoft.OpenApi.Models;
+
+namespace FinancialGoalManager.API.Extensions
+{
+    public static class ServiceConfiguration
+    {
+        public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.ConfigureDependencyInjection()
+                .ConfigureOthersServices();
+
+            builder.Services.AddControllers();
+
+            // swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "FinancialGoalManager.API",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Matheus Wylliam",
+                        Email = "matheus.wylliam@edu.pucrs.br",
+                        Url = new Uri("https://github.com/MatheusWylliam")
+                    }
+                });
+            });
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder ConfigureDependencyInjection(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IFinancialGoalRepository, FinancialGoalRepository>();
+            builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+            builder.Services.AddScoped<IReportsRepository, ReportsRepository>();
+
+            return builder;
+        }
+
+        public static WebApplicationBuilder ConfigureOthersServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(typeof(RegisterGoalCommand).Assembly);
+            });
+
+            return builder;
+        }
+    }
+}
