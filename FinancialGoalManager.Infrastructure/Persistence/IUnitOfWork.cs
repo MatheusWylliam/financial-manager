d3dde609a6f43926cca7797a404cd9ad99545be5
Matheus <Hazelramahz@icloud.com>
2026-05-20 19:54:20 -0300

    feat(infrastructure): add explicit transaction control with begin, comm and rollback

--- FinancialGoalManager.Infrastructure/Persistence/IUnitOfWork.cs
+++ FinancialGoalManager.Infrastructure/Persistence/IUnitOfWork.cs
@@ -2,11 +2,13 @@ using FinancialGoalManager.Core.Repositories;
 
 namespace FinancialGoalManager.Infrastructure.Persistence
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
         IFinancialGoalRepository FinancialGoalRepository { get; }
         IReportsRepository ReportsRepository { get; }
         ITransactionRepository TransactionRepository { get; }
+        Task BeginTransactionAsync();
+        Task CommitAsync();
         Task<int> CompleteAsync();
     }
 }
