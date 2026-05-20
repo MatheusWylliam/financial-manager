c6f1878e1ea12423ada1f7fddccced3f928ef037
Matheus <Hazelramahz@icloud.com>
2026-05-19 23:00:23 -0300

    feat: add final amount simulation and fix command handlers returns

--- FinancialGoalManager.Application/Commands/FinancialGoals/UploadCover/UploadCoverCommand.cs
+++ FinancialGoalManager.Application/Commands/FinancialGoals/UploadCover/UploadCoverCommand.cs
@@ -2,7 +2,7 @@ using MediatR;
 
 namespace FinancialGoalManager.Application.Commands.FinancialGoals.UploadCover
 {
-    public class UploadCoverCommand : IRequest
+    public class UploadCoverCommand : IRequest<bool>
     {
         public int Id { get; set; }
         public byte[] Cover { get; set; }
